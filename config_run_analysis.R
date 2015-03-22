627518c6869d899b349b3c94b528c2518132b8f0
Omar Velazquez <ovelazquez@convergiq.com>
2015-03-22 01:55:41 -0600

    preparing script and MD files

---
+++ config_run_analysis.R
@@ -0,0 +1,26 @@
+##
+## Global variables
+## do not change this values, are used by the Script to perform the main functionality
+dirSeparator<-"/"
+urlDataSource<<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipFileName<<-"UCI_HAR_Dataset.zip"
+workingDirectory<<-paste(".",dirSeparator,sep="")
+datasetsDirectory <<-paste("UCI HAR Dataset",dirSeparator,sep="")
+datasetFileType<-".txt"
+
+###
+feautresInfoFile <- paste("features_info",datasetFileType,sep="")
+featuresFile <- paste("features",datasetFileType,sep="")
+activityLabels <- paste("activity_labels",datasetFileType,sep="")
+trainId<-"train"
+testId<-"test"
+trainDir<-paste(trainId,"/",sep="")
+testDir<-paste(testId,"/",sep="")
+sets <- "X_"
+labels <- "y_"
+
+### Error messages
+readmeFile<<-"please go to README.md file for more info"
+zipError<<-"Data source does not exists"
+dirError<<-"Directory UCI HAR Dataset does not exists, trying to generate it"
+fileError<<-paste(": file does not exists, ",readmeFile)
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,83 @@
 ##
 ## run_analysis
 ## Author: Omar Velazquez <ovelazquezj@gmail.com>
-##
+
+## Run config values, in order to keep clean this script, I have created a configuration file
+## by separately to help you to customize the use of run_analizis script, please README file
+## for more details and examples
+source("config_run_analysis.R")
+
+## Initializing working directories and Dataset files, 
+## this function should validate your Working directory structure also Dataset sources
+## if not exists, then whould try to download and run a unzip
+initialize <- function(){
+    prepareDirectoryStructure()    
+}
+prepareDirectoryStructure<-function(){
+    if(!existDataSourceDir()){
+        unzipDatasetFileSource()
+    }
+    print("READY TO GO!",,quote=FALSE)
+}
+existDataSourceDir<-function(){
+    if(!fileDirValidation(datasetsDirectory)){
+        print(dirError,quote=FALSE)
+        FALSE
+    }else{
+        TRUE
+    }
+}
+existsDataSourceFile<-function(){
+    if(!fileDirValidation(zipFileName)){
+        print(zipError,quote=FALSE)
+        FALSE
+    }else{
+        TRUE
+    }
+}
+unzipDatasetFileSource<-function(){
+    if(!existsDataSourceFile()){
+        downloadDataSource()
+    }
+    print(paste("Extracting dataset source directory in: ",workingDirectory, datasetsDirectory),quote=FALSE)
+    result<-tryCatch({
+        unzip(zipFileName)
+    },warning=function(warn){
+        print(paste("UNZIP_WARNING: " , warn),quote=FALSE)
+    },error=function(err){
+        print(paste("UNZIP_ERROR: " , err,"\\n Please Try to unzip manually"),quote=FALSE)
+        print(readmeFile,quote=FALSE)
+    },finaly={
+        print("Extracting file",quote=FALSE)
+    }) 
+}
+downloadDataSource<-function(){
+    result<-tryCatch({
+        download.file(urlDataSource,zipFileName,"curl")    
+    },warning=function(warn){
+        print(paste("DOWNLOAD_WARNING: " , warn),quote=FALSE)
+    },error=function(err){
+        print(paste("DOWNLOAD_ERROR: " , err,"\n Try to download and unzip manually"),quote=FALSE)
+        print(readmeFile,quote=FALSE)
+    },finaly={
+        print("Starting download",quote=FALSE)
+    }) 
+}
+validateTrainingDirectory<-function(){
+    
+}
+validateTestDirectory<-function(){
+    
+}
+
+readFileSource <- function(fileName){
+
+    
+}
+fileDirValidation<-function(sourceName){
+    if(file.exists(sourceName)){
+        TRUE
+    }else{
+        FALSE
+    }
+}
